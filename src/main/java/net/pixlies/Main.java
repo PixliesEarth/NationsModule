66c7dcdbe91f0c2fa270221bf8cbed71c98e71b9
TAMaurice <schreiner.maurice@outlook.com>
2022-01-20 19:06:46 +0100

    Added @Overrides to Main

--- src/main/java/net/pixlies/Main.java
+++ src/main/java/net/pixlies/Main.java
@@ -3,10 +3,13 @@ package net.pixlies;
 import net.pixlies.modules.Module;
 
 public class Main extends Module {
+
+    @Override
     public void onLoad() {
         System.out.println("Loaded this successfully");
     }
 
+    @Override
     public void onDrop() {
         System.out.println("Dropped this successfully");
     }
